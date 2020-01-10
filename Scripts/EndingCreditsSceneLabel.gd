aa40b60af60ef03caad2c2a0f7dfbb7b2dd79b4a
RobertKopczynski <robertkop89@gmail.com>
2020-01-10 21:24:13 +0100

    Updated bullet speed +50%

--- Scripts/EndingCreditsSceneLabel.gd
+++ Scripts/EndingCreditsSceneLabel.gd
@@ -4,6 +4,7 @@ func _ready():
     set_visible_characters(0)
     set_process_input(true)
 
+# warning-ignore:unused_argument
 func _input(event):
     if Input.is_action_just_released("ui_select"):
         if get_visible_characters() >= get_total_character_count():
